dfe17a67243ac8887312024599b21497b73ccefc
Natsu1270 <hungduy1270@gmail.com>
2020-01-15 16:57:10 +0700

    nothing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.style.scss'
 
 const MenuItem = ({ title, imageUrl, history, linkUrl, match }) => (
-    <div className="menu-item" onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div className="menu-item" onClick={() => history.push(`${match.url}shop/${linkUrl}`)}>
         <div className="bg-img" style={{ backgroundImage: `url(${imageUrl})` }}>
         </div>
         <div className="content">
--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -6,15 +6,27 @@ import { useSelector } from 'react-redux'
 
 import { selectCollection } from '../../redux/shop/shop.selector'
 
+
 import './category.style.scss'
 
 export default function CategoryPage({ match }) {
-    console.log(match)
     const collection = useSelector((state) => selectCollection(match.params.categoryId)(state))
-    console.log(collection)
-    return (
-        <div className="category-page">
-            Category
-        </div>
-    )
+    if (collection) {
+        return (
+            <div className="category-page">
+                <h1>{collection.title}</h1>
+                <div className="collection-items">
+                    {
+                        collection.items.map(item => (
+                            <CollectionItem item={item} />
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <h1>Category not exist</h1>
+        )
+    }
 }
